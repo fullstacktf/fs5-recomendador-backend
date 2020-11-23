6cdc87ad52154634bdd7bd8c91efd6ef57767dbb
japmelian <japmelian@gmail.com>
2020-11-23 20:02:08 +0000

    Database refactor; create and populate MongoDB database

---
+++ database/scripts/01-insertData.sh
@@ -0,0 +1,3 @@
+mongoimport --db urecommendme --collection media --file data/init-data/media.json
+mongoimport --db urecommendme --collection users --file data/init-data/users.json
+mongoimport --db urecommendme --collection tags --file data/init-data/tags.json
