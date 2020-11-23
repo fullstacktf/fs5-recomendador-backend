35bd57b9d5182db5f777e76375269a5386c58dc4
japmelian <japmelian@gmail.com>
2020-11-23 20:19:45 +0000

    Fix error on NodeJS: new port added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+const PORT = 80;
+
 app.get('/', (req, res) => {
     res.json({message: "Welcome to uRecommend.me!"})
 });
+
+app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
